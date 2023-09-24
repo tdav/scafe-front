db251ef3827b4dd2eed5258e90e548a492b0c36e
jasur-kasimov <kasimojasur75@gmail.com>
2023-09-24 14:36:09 +0500

    11

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Fixedavbar</a>
+          <a class="navbar-brand" href="#">my sCafe</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
