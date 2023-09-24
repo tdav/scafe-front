8b40d0e06f7b998698c4b37dddfc4da3a98e2e02
Tasibaev Davron <tdav@mail.ru>
2023-09-24 12:11:10 +0500

    dash

--- src/App.vue
+++ src/App.vue
@@ -35,21 +35,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </header>
     
-    <!-- Begin page content -->
-    <main class="flex-shrink-0">
-      <div class="container">
-        <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-        <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-        <p>Back to <a href="/docs/5.0/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
-      </div>
-    </main>
-    
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
-      </div>
-    </footer>
-    
+
     
 
 
