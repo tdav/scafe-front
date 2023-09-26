4ac336b3f7a50b1369350f1c69fb264ed651305e
jasur-kasimov <kasimojasur75@gmail.com>
2023-09-26 23:25:39 +0500

    qo'shildi

--- src/router.js
+++ src/router.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from './view/login.vue'
 import Dashboard from './view/dashboard/Dashboard.vue'
+import AddNote from './view/dashboard/add _note.vue'
+import AddNotee from './view/dashboard/add _notee.vue'
+import AdditonalOrder from './view/dashboard/additonal_order.vue'
+import DinningOption from './view/dashboard/dinning_option.vue'
+import OrderConfirmation from './view/dashboard/order_confirmation.vue'
+import Payment from './view/dashboard/payment.vue'
+import SelectTable from './view/dashboard/select_table.vue'
+import TipAmount from './view/dashboard/tip_amount.vue'
 import LoginCashierOrm from './view/pos/login_cashier_orm.vue'
-import GuestDetail from './view/reservation/guest_detail.vue'
-import List from './view/reservation/list.vue'
+import LoginCashier from './view/pos/login_cashier.vue'
+import UpcomingOrders from './view/pos/UpcomingOrders.vue'
+import WalkInAbsent from './view/pos/walk_in_absent.vue'
+import WalkInLoginCashier from './view/pos/walk_in_login_cashier.vue'
 
 const routes = [
     {
@@ -17,20 +27,70 @@ const routes = [
         component: Dashboard
     },
     {
+        path: '/AddNote',
+        name: 'AddNote',
+        component: AddNote
+    },
+    {
+        path: '/AddNotee',
+        name: 'AddNotee',
+        component: AddNotee
+    },
+    {
+        path: '/AdditonalOrder',
+        name: 'AdditonalOrder',
+        component: AdditonalOrder
+    },
+    {
+        path: '/DinningOption',
+        name: 'DinningOption',
+        component: DinningOption
+    },
+    {
+        path: '/OrderConfirmation',
+        name: 'OrderConfirmation',
+        component: OrderConfirmation
+    },
+    {
+        path: '/Payment',
+        name: 'Payment',
+        component: Payment
+    },
+    {
+        path: '/SelectTable',
+        name: 'SelectTable',
+        component: SelectTable
+    },
+    {
+        path: '/TipAmount',
+        name: 'TipAmount',
+        component: TipAmount
+    },
+    {
         path: '/LoginCashierOrm',
-        name:'LoginCashierOrm',
-        component: LoginCashierOrm 
+        name: 'LoginCashierOrm',
+        component: LoginCashierOrm
     },
     {
-        path: '/GuestDetail',
-        name: 'GuestDetail',
-        component: GuestDetail
+        path: '/LoginCashier',
+        name: 'LoginCashier',
+        component: LoginCashier
     },
     {
-        path: '/List',
-        name: 'List',
-        component: List
-    }
+        path: '/UpcomingOrders',
+        name: 'UpcomingOrders',
+        component: UpcomingOrders
+    },
+    {
+        path: '/WalkInAbsent',
+        name: 'WalkInAbsent',
+        component: WalkInAbsent
+    },
+    {
+        path: '/WalkInLoginCashier',
+        name: 'WalkInLoginCashier',
+        component: WalkInLoginCashier
+    },
 ]
 
 const router = createRouter({ history: createWebHistory(), routes })
