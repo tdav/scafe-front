09defaf5b958d85a85dc9b27dbd3de48dd643c28
jasur-kasimov <kasimojasur75@gmail.com>
2023-10-29 13:54:36 +0500

    l

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <router-link to="/">Home</router-link>
         <router-link to="/order">Order</router-link>
         <router-link to="/history">History</router-link>
-
+        <img class="" src="assets/imgs/Nav_Profile" alt="">
         <router-view></router-view>
     </div>
 </template>
