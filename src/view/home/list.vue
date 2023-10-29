03e0a05f9fad57802babc6af7a162d72959872ba
azizbek-isakboyev <isakboyevazizbek@gmail.com>
2023-10-29 15:52:53 +0500

    111

--- src/view/home/list.vue
+++ src/view/home/list.vue
@@ -1,31 +1,30 @@
 <template >
-    <div>
-        <button class="btn btn-body">
-            <img src="../../assets/imgs/btn/offers.png" alt="">
-            <h3 class="h3-1">Coffee</h3>
-        </button>
-        <button class="btn btn-body">
-            <img src="../../assets/imgs/btn/beverages.png" alt="">
-            <h3 class="h3-1">Beverages</h3>
-        </button>
-        <button class="btn btn-danger">
-            <img src="../../assets/imgs/btn/breakfast.png" alt="">
-            <h3 class="h3-1">Food</h3>
-        </button>
-        <button class="btn btn-body">
-            <img src="../../assets/imgs/btn/pasta.png" alt="">
-            <h3 class="h3-1">Appetizer</h3>
-        </button>
-        <button class="btn btn-body">
-            <img src="../../assets/imgs/btn/lasagna.png" alt="">
-            <h3 class="h3-1">Bread</h3>
-        </button>
-        <button class="btn btn-body">
-            <img src="../../assets/imgs/btn/misc.png" alt="">
-            <h3 class="h3-1">Snack</h3>
-        </button>
-        <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
- 
+    <div class="col-1 bg-body border-end" style="width: 125px; border-width: 1px; border-color: #E4E4E4;">
+        <div>
+            <button class="btn btn-body btn-1">
+                <img src="../../assets/imgs/btn/offers.png" alt="">
+                <h3 class="h3-1">Coffee</h3>
+            </button> <br>
+            <button class="btn btn-body btn-1">
+                <img src="../../assets/imgs/btn/beverages.png" alt="">
+                <h3 class="h3-1">Beverages</h3>
+            </button> <br>
+            <button class="btn btn-body btn-1">
+                <img src="../../assets/imgs/btn/breakfast.png" alt="">
+                <h3 class="h3-1">Food</h3>
+            </button> <br>
+            <button class="btn btn-body btn-1">
+                <img src="../../assets/imgs/btn/pasta.png" alt="">
+                <h3 class="h3-1">Appetizer</h3>
+            </button> <br>
+            <button class="btn btn-body btn-1 active">
+                <img src="../../assets/imgs/btn/lasagna.png" alt="">
+                <h3 class="h3-1">Bread</h3>
+            </button> <br>
+            <button class="btn btn-body btn-1">
+                <img src="../../assets/imgs/btn/misc.png" alt="">
+                <h3 class="h3-1">Snack</h3>
+            </button>
         </div>
     </div>
 </template>
@@ -38,6 +37,7 @@
 <style scoped>
 
 .h3-1{
+    text-align: center;
     color: #828487;
     font-family: Poppins;
     font-size: 12px;
@@ -45,6 +45,38 @@
     line-height: 14px;
     letter-spacing: 0px;
     text-align: center;
+}
+
+.btn-1 {
+    margin: 10px;
+    border-radius: 20px; 
+    width:  77px; 
+    height: 78px; 
+    padding: 12px, 6px, 12px, 6px; 
+    border-radius: 8px; gap: 8px;
+}
+
+.btn-1:active {
+    background-color: #FFAB18;
+    margin: 10px;
+    border-radius: 8px; gap: 8px;
+    width: 77px;
+    height: 78px;
+    padding: 8px, 6px, 8px, 6px;
+    border-radius: 8px;
+    gap: 8px;
+}
+
+
+.active {
+    background-color: #FFAB18 !important;
+    margin: 10px;
+    border-radius: 8px; gap: 8px !important;
+    width: 77px;
+    height: 78px;
+    padding: 8px, 6px, 8px, 6px;
+    border-radius: 8px;
+    gap: 8px;
 }
       
 </style>
