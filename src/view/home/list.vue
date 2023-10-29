0ec8add3cadf6aac29724da72b151212bd860c53
azizbek-isakboyev <isakboyevazizbek@gmail.com>
2023-10-29 13:35:49 +0500

    l

--- src/view/home/list.vue
+++ src/view/home/list.vue
@@ -1,22 +1,27 @@
 <template >
     <div>
         <button class="btn btn-body">
-            <img src="../../assets/imgs/btn img/" alt="">
+            <img src="../../assets/imgs/btn/offers.png" alt="">
             <h3 class="h3-1">Coffee</h3>
         </button>
         <button class="btn btn-body">
+            <img src="../../assets/imgs/btn/beverages.png" alt="">
             <h3 class="h3-1">Beverages</h3>
         </button>
-        <button class="btn btn-body">
+        <button class="btn btn-danger">
+            <img src="../../assets/imgs/btn/breakfast.png" alt="">
             <h3 class="h3-1">Food</h3>
         </button>
         <button class="btn btn-body">
+            <img src="../../assets/imgs/btn/pasta.png" alt="">
             <h3 class="h3-1">Appetizer</h3>
         </button>
         <button class="btn btn-body">
+            <img src="../../assets/imgs/btn/lasagna.png" alt="">
             <h3 class="h3-1">Bread</h3>
         </button>
         <button class="btn btn-body">
+            <img src="../../assets/imgs/btn/misc.png" alt="">
             <h3 class="h3-1">Snack</h3>
         </button>
     </div>
