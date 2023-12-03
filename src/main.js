20637ff8909fec02b0cbd1a32c01863f5d384b27
jasur-kasimov <kasimojasur75@gmail.com>
2023-12-03 15:11:34 +0500

    a

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
