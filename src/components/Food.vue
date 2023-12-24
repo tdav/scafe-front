aafbbffff91649e692042ef761542ea2ba5c1aad
Davron <tdavron@gmail.com>
2023-12-24 20:45:37 +0500

    component

---
+++ src/components/Food.vue
@@ -0,0 +1,29 @@
+<template >
+
+    
+    <div class="eat1-3 col-2 mx-1 mt-2" style="border: 1px;">
+        <img class="eat1-3-1 mt-2 ms-1" :src="fimg">
+        <p>{{ fname }}</p>
+        <p>{{ fdescription }}</p>
+    </div>
+</template> 
+
+<script setup>
+
+
+
+import { defineProps } from 'vue';
+
+const props = defineProps(['fid', 'fname', 'fdescription', 'fimg'])
+
+
+console.log(props.fdescription);
+
+function mySelectEts(i) {
+    console.log(i);
+}
+
+</script>
+
+
+<style scoped></style>
