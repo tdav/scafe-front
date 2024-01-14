7c8307c3e44e683182dd753abf38a5b871e55f25
jasur-kasimov <kasimojasur75@gmail.com>
2024-01-14 16:34:39 +0500

    a

--- src/components/OrderListItem.vue
+++ src/components/OrderListItem.vue
@@ -1,20 +1,26 @@
 <template >
-    <div class="row">
+    <div class="container-fluid p-0">
+        <div class="row" style="height: 100px;">
+            <img class="col-4 eat1-3-11 mt-2 ms-1 p-0" :src="fimg" style="height: 80px; width: 80px; display: inline-block;">
+            <div class="col-6 p-0">
+                <p class="ms-1" style="margin: 0px; font-size: 20px;"> {{ fname }}</p>
+                <p class="p-0 ms-3" style="margin: 0px; font-size: 13px;">Qty {{ count }}</p>
+                <p class="p-0 ms-3" style="margin: 0px; font-size: 13px;">Price {{ fprice }}</p>
+                <p class="ms-3" style="margin: 0px; font-size: 15px;">Total: {{ jamiSumma }} so'm</p>
+            </div>
+            <div class="col-3 p-0">
+                <div style="margin-top: 30px;">
+                <button @click="EtsPlus()" class="my-btn-plus m-0" style="width: 35px; display: inline-block;">
+                    <p class="parag  m-0">+</p>
+                </button>
+                
+                <button @click="EtsMinus()" class="my-btn-minus m-0" style="width: 35px; display: inline-block;">
+                    <p class="parag  m-0">-</p>
+                </button>
+                </div>
 
-        <img class="eat1-3-11 mt-2 ms-1" :src="fimg">
-        <p> {{ fname }}</p>
-        <p>Price {{ fprice }}</p>
-        <p>Qty {{ count }}</p>
-        <p>Total {{ jamiSumma }}</p>
-
-        <button @click="EtsPlus()" class="my-btn-plus">
-            <p class="parag  m-0">+</p>
-        </button>
-
-        <button @click="EtsMinus()" class="my-btn-minus">
-            <p class="parag  m-0">-</p>
-        </button>
-
+            </div>
+        </div>
     </div>
 </template>
 
