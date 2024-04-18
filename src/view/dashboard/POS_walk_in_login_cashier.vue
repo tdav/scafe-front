47af0203d9b51a0fae38f7104d5caf2fa0010dc7
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-04-18 19:38:39 +0300

    yjtbkyuglbh

--- src/view/dashboard/POS_walk_in_login_cashier.vue
+++ src/view/dashboard/POS_walk_in_login_cashier.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="container asd">
-    
-  </div>
+  <div class="container asd"></div>
 </template>
 
 <style>
-  .asd {
-    background-image: url(../public/assets/images/Background.absent.png);
-    width: 100px;
-    height: 800px;
-    background-repeat: no-repeat;
-  }
+.asd {
+  background-image: url(../public/assets/images/Background.absent.png);
+  width: 100px;
+  height: 800px;
+  background-repeat: no-repeat;
+}
 </style>
