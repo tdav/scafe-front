f20f519e89feda2f9fab3c29f6f970a787f58cda
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-04-18 19:29:29 +0300

    asd

--- src/view/dashboard/POS_walk_in_login_cashier.vue
+++ src/view/dashboard/POS_walk_in_login_cashier.vue
@@ -1,54 +1,14 @@
 <template>
-    <div>
-      <!-- Soat ko'rsatiladigan div -->
-      <div>{{ formattedTime }}</div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        // Boshlang'ich vaqt
-        startHour: 0, // soat
-        startMinute: 0, // minut
-        startSecond: 0 // sekund
-      };
-    },
-    computed: {
-      // 24 soatlik davrni hisoblash uchun funksiya
-      newTime() {
-        // Boshlang'ich vaqt
-        let startTime = this.startHour * 3600 + this.startMinute * 60 + this.startSecond;
-        // Yakun vaqti
-        let endTime = startTime + 24 * 3600;
-        
-        // Yangi kunning boshlanish vaqti
-        let newStartTime = endTime % (24 * 3600);
-        let newStartHour = Math.floor(newStartTime / 3600);
-        let remainingSeconds = newStartTime % 3600;
-        let newStartMinute = Math.floor(remainingSeconds / 60);
-        let newStartSecond = remainingSeconds % 60;
-        
-        // Yangi vaqtni qaytarish
-        return { hour: newStartHour, minute: newStartMinute, second: newStartSecond };
-      },
-      // Formatlangan soat
-      formattedTime() {
-        // Yangi kunning soati, minutlari va sekundlari
-        let newHour = this.newTime.hour;
-        let newMinute = this.newTime.minute;
-        let newSecond = this.newTime.second;
-        
-        // Formatlangan vaqt (HH:MM:SS)
-        let formattedHour = newHour < 10 ? '0' + newHour : newHour;
-        let formattedMinute = newMinute < 10 ? '0' + newMinute : newMinute;
-        let formattedSecond = newSecond < 10 ? '0' + newSecond : newSecond;
-        
-        // Formatlangan vaqt
-        return `${formattedHour}:${formattedMinute}:${formattedSecond}`;
-      }
-    }
-  };
-  </script>
-  
+  <div class="container asd">
+    
+  </div>
+</template>
+
+<style>
+  .asd {
+    background-image: url(../public/assets/images/Background.absent.png);
+    width: 100px;
+    height: 800px;
+    background-repeat: no-repeat;
+  }
+</style>
