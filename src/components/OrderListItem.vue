639c943858004133b5882c9a6914e9f6566b7f6e
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-04-30 20:46:31 +0500

    JUYTRE

--- src/components/OrderListItem.vue
+++ src/components/OrderListItem.vue
@@ -1,23 +1,24 @@
-<template >
+<template>
     <div class="container-fluid p-0">
         <div class="row mb-1 rro border border-1">
 
             <img class="col-4 eat1-3-11 mt-2 ms-1 p-0" :src="props.foodData.imgUrl"
                 style="height: 80px; width: 80px; display: inline-block;">
 
-            <div class="col-6 p-0">
+            <div class="col-5 p-0">
                 <p class="ms-1" style="margin: 0px; font-size: 13px;"> <b>{{ props.foodData.name }}</b></p>
-                <p class="p-0 ms-3" style="margin: 0px; font-size: 13px;">Dona: {{ props.foodData.count }}</p>
                 <p class="p-0 ms-3" style="margin: 0px; font-size: 13px;">Narxi: {{ props.foodData.price }}</p>
                 <p class="ms-3" style="margin: 0px; font-size: 15px;">Totol: {{ jamiSumma }} so'm</p>
             </div>
 
-            <div class="col-3 p-0">
-                <div style="margin-top: 30px;">
+            <div class="col-4 p-0">
+                <div style="margin-top: 30px; margin-left: 10px;">
                     <button @click="EtsPlus()" class="my-btn-plus m-0"
                         style="width: 35px; float: left; display: inline-block; background: linear-gradient(218.57deg, rgb(255, 238, 0) -50%, rgba(255, 136, 0, 0.425) 137.69%);">
                         <p class="parag  m-0">+</p>
                     </button>
+
+                    <p class="p-0 ms-1" style="margin: 0px; float: left; margin-top: 8px; font-size: 13px;">{{ props.foodData.count }}</p>
 
                     <button @click="EtsMinus()" class="my-btn-minus m-0 ms-2"
                         style="width: 35px; float: left; display: inline-block; background: linear-gradient(218.57deg, rgb(255, 238, 0) -50%, rgba(255, 136, 0, 0.425) 137.69%);">
@@ -34,10 +35,10 @@
 <script setup>
 import { ref, defineProps, defineEmits, onMounted } from 'vue';
 
-const props = defineProps( { 
+const props = defineProps({
     foodData: {
-        type:Object,
-        required:true,
+        type: Object,
+        required: true,
     }
 });
 
@@ -49,7 +50,7 @@ const emit = defineEmits(['onChangeCount'])
 
 onMounted(() => {
     props.foodData['count'] = 1;
-    jamiSumma.value = props.foodData.price * props.foodData.count;    
+    jamiSumma.value = props.foodData.price * props.foodData.count;
 })
 
 function EtsPlus() {
@@ -64,10 +65,10 @@ function EtsMinus() {
     if (props.foodData.count > 0) {
         props.foodData.count--;
         this.jamiSumma = props.foodData.price * props.foodData.count;
-    
+
         emit('onChangeCount', props.foodData)
-        
-        
+
+
     } else {
         return;
     }
@@ -75,7 +76,7 @@ function EtsMinus() {
 
 </script>
 
-<style >
+<style>
 .rro {
     height: 100px;
 }
