870ac8de59f2536b0f9ba0e33b5d0d28742f7399
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-05-05 14:35:15 +0500

    rjd

--- src/view/new/region.vue
+++ src/view/new/region.vue
@@ -1,21 +1,39 @@
 <template>
-    <div class="mt-5">
-        <p>kljhhhhhhhhhhhhhbiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</p>
+    <div class="home">
+        <h3>Table page</h3>
+        <button @click="OnLoad"> Load Data</button>
+        <table class="table  table-dark table-striped">
+            <thead>
+                <tr class="row">
+                    <th class="col-2">#</th>
+                    <th class="col-10" style="text-align: center;">NameUz</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="it in myregion" :key="it.id">
+                    <td class="table-primary">{{ it.status }}</td>
+                    <td class="table-primary">{{ it.nameEn }}</td>
+                    <td class="table-primary">{{ it.nameLt }}</td>
+                    <td class="table-primary">{{ it.nameRu }}</td>
+                    <td class="table-primary">{{ it.nameUz }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
-<script>
+<script setup>
 import { ref, onMounted } from 'vue';
-import DataService from "../../services/data.service"
-let myregion = []
+import DataService from '../../services/data.service'
 
-onMounted(() => {
+
+let myregion = ref([])
+
+function OnLoad() {
 
     DataService.getregion().then((response) => {
-         
-        foodData.value = response.data;
-        myregion = response.data; 
+        myregion.value = response.data;
     })
 
-})
+}
 
 </script>
